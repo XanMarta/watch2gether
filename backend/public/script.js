8da7495432ed00724f143184a2b125c6fead0db2
dangbb <19020033@vnu.edu.vn>
2022-03-25 01:41:47 +0700

    Runnable Web

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,74 +1,152 @@
-const videoGrid = document.getElementById("video-grid");
-const myVideo = document.createElement("video");
-const socket = io('/')
-var myVideoStream;
+// const videoGrid = document.getElementById("video-grid");
+// const myVideo = document.createElement("video");
+// const socket = io()
+// var myVideoStream;
 
-// Create and receive connection
-var peer = new Peer(undefined, {
-  path: "/peerjs",
-  host: "/",
-  port: "3030"
-});
-myVideo.muted = true;
+// // Create and receive connection
+// var peer = new Peer(undefined, {
+//   path: "/peerjs",
+//   host: "/",
+//   port: "3030"
+// });
+// myVideo.muted = true;
 
-console.log("Create new peer: ", peer)
+// console.log("Create new peer: ", peer)
 
 
-const connectToNewUser = (userId, stream) => {
-  console.log("Connect to new user: ", userId)
+// const connectToNewUser = (userId, stream) => {
+//   console.log("Connect to new user: ", userId) 
 
-  const call = peer.call(userId, stream)
-  const video = document.createElement("video")
+//   const call = peer.call(userId, stream)
+//   const video = document.createElement("video")
 
-  call.on("stream", (userVideoStream) => {
-    console.log("Call got a answer stream")
-    addVideoStream(video, userVideoStream)
-  })
+//   call.on("stream", (userVideoStream) => {
+//     console.log("Call got a answer stream")
+//     addVideoStream(video, userVideoStream)
+//   })
+// }
+
+// peer.on("open", (id) => {
+//   console.log("Peer got open room id: ", ROOM_ID)
+//   console.log("Peer got open user id: ", id)
+
+//   console.log("Emit join-room")
+//   console.log("Emit socket id: ", socket.id)
+//   console.log("Socket: ", socket)
+//   socket.emit("join-room", ROOM_ID, id)
+// })
+
+// const addVideoStream = (video, stream) => {
+//   video.srcObject = stream;
+//   video.addEventListener("loadedmetadata", () => {
+//      video.play();
+//      videoGrid.append(video);
+//   });
+// };
+
+
+// navigator.mediaDevices.getUserMedia({
+//   audio: true,
+//   video: true,
+// })
+// .then((stream) => {
+//   myVideoStream = stream;
+//   addVideoStream(myVideo, stream);
+
+//   peer.on("call", (call) => {
+//     console.log("Peer got a call")
+
+//     call.answer(stream);
+
+//     const video = document.createElement("video")
+
+//     call.on("stream", (userVideoStream) => {
+//       console.log("Call got a stream")
+//       addVideoStream(video, userVideoStream)
+//     })
+//   })
+
+//   socket.on("user-connected", (userId) => {
+//     connectToNewUser(userId, stream);
+//   })
+
+//   console.log("Emit message ready")
+//   socket.emit("ready")
+// });
+
+
+const socket = io();
+
+// Define reference to UI element
+const loginBtn = document.querySelector("#loginBtn")
+const localVideo = document.querySelector("#localVideo")
+const remoteVideo = document.querySelector("#remoteVideo")
+const usernameInput = document.querySelector("#usernameInput")
+const callToUsernameInput = document.querySelector("#callToUsernameInput")
+const callBtn = document.querySelector("#callBtn")
+
+// Get right to use camera 
+var myStream;
+const userMediaConstraint = {
+  video: true
 }
 
-peer.on("open", (id) => {
-  console.log("Peer got open room id: ", ROOM_ID)
-  console.log("Peer got open user id: ", id)
+// Current username 
+let userName = "";
 
-  console.log("Emit join-room")
-  console.log("Emit socket id: ", socket.id)
-  socket.emit("join-room", ROOM_ID, id)
-})
+// Function called when new user is connected
+const newUserConnected = (user) => {
+  console.log("Create new user")
 
-const addVideoStream = (video, stream) => {
-  video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
-     video.play();
-     videoGrid.append(video);
-  });
+  userName = usernameInput.value;
+
+  if (userName.length > 0) {
+      socket.emit("new user", userName);
+      addToUsersBox(userName);
+  }
 };
 
+// Function logs all current online username
+const addToUsersBox = (userName) => {
+    console.log(`* ${userName}`)
+};
 
-navigator.mediaDevices.getUserMedia({
-  audio: true,
-  video: true,
-})
-.then((stream) => {
-  myVideoStream = stream;
-  addVideoStream(myVideo, stream);
+// Function call when want to connect to other user
+const connectToUser = () => {
+  var userToConnect = callToUsernameInput.value;
 
-  peer.on("call", (call) => {
-    console.log("Peer got a call")
+  if (userToConnect.length > 0) {
+    socket.emit("connect to user", `Hello from ${userName}`)
+  }
+}
 
-    call.answer(stream);
 
-    const video = document.createElement("video")
-
-    call.on("stream", (userVideoStream) => {
-      console.log("Call got a stream")
-      addVideoStream(video, userVideoStream)
+// new user is created so we get nickname and emit event
+loginBtn.addEventListener("click", (event) => {
+  navigator.mediaDevices.getUserMedia(userMediaConstraint)
+    .then((stream) => {
+      myStream = stream;
+      localVideo.srcObject = stream;
     })
-  })
+    .catch((err) => console.log("WebRTC get error: ", err))
 
-  socket.on("user-connected", (userId) => {
-    connectToNewUser(userId, stream);
-  })
+  newUserConnected();
+})
 
-  console.log("Emit message ready")
-  socket.emit("ready")
+// Connect to a specific user
+callBtn.addEventListener("click", (event) => {
+  connectToUser();
+})
+
+// Whenever get a new user, logs all current online user
+socket.on("new user", function (data) {
+    console.log("Current user list:")
+
+    data.map((data) => addToUsersBox(data));
 });
+
+// Call when an user disconnected
+socket.on("user disconnected", function (userName) {
+    console.log(`User ${userName} disconnected.`)
+});
+
--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,133 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
+// const express = require('express');
+// const app = express();
+// const server = require('http').Server(app);
+// const { v4: uuidv4 } = require("uuid");
+// const io = require("socket.io")(server)
+// const {ExpressPeerServer} = require("peer");
+// const { SocketAddress } = require('net');
+
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true
+// })
+
+// app.set('view engine', 'ejs')
+
+// app.use(express.static('public'))
+// app.use("/peerjs", peerServer)
+
+// app.get('/', (req, res) => {
+//   res.redirect(`/${uuidv4()}`);
+// });
+
+
+// app.get('/:room', (req, res) => {
+//   res.render('room', { 
+//     roomId: req.params.room
+//   });
+// })
+
+// io.on("connection", (socket) => {
+
+//   socket.on("connect-error", (err) => {
+//     console.log("Connect_error due to ", err.message)
+//   })
+
+//   socket.on("join-room", (roomId, userId) => {
+//     console.log("Server get join request")
+//     console.log("Room id: ", roomId)
+//     console.log("User id: ", userId)
+//   })
+
+//   socket.on('ready', () => {
+//     console.log("Socket get ready")
+//   })
+
+//   socket.on("join-room", (roomId, userId) => {
+//     console.log("1. Join room userId: ", userId)
+//     console.log("2. Join room roomId: ", roomId)
+//     console.log("3. Add ready and disconnect to socket")
+
+//     socket.join(roomId)
+//     io.broadcast.to(roomId).emit('user-connected', userId)
+
+//     socket.on('disconnect', () => {
+//       console.log("Socket disconnected")
+//       io.to(roomId).emit('user-disconnected', userId)
+//     })
+//   })
+// })
+
+// server.listen(3030);
+
+
+const express = require("express");
+const socket = require("socket.io");
 const { v4: uuidv4 } = require("uuid");
-const io = require("socket.io")(server)
-const {ExpressPeerServer} = require("peer");
 
-const peerServer = ExpressPeerServer(server, {
-  debug: true
-})
 
+// App setup
+const PORT = 5000;
+const app = express();
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+// Static files
+app.use(express.static("public"));
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
-app.use("/peerjs", peerServer)
+// Socket setup
+const io = socket(server);
 
+const activeUsers = new Set();
+
+// redirect client to a room URL
 app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
-
+// API for a room. Render page and extract roomID
 app.get('/:room', (req, res) => {
   res.render('room', { 
     roomId: req.params.room
   });
 })
 
-io.on("connection", (socket) => {
-  console.log("1. Server connection")
-  console.log("2. Socket id: ", socket.id)
+// Called whenever get a new connection
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
 
-  socket.on("join-room", (roomId, userId) => {
-    console.log("1. Join room userId: ", userId)
-    console.log("2. Join room roomId: ", roomId)
-    console.log("3. Add ready and disconnect to socket")
+  // called whenever get a new user
+  socket.on("new user", function (data) {
+    console.log("New user")
+
+    socket.userId = data;
+    activeUsers.add(data);
+
+    // server send broadcast to all client
+    io.emit("new user", [...activeUsers]);
+  });
+
+  // call when an connection is disconnected
+  socket.on("disconnect", () => {
+    console.log("Disconnected")
+
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
+  });
+
+  // call when an connection want to join a room 
+  // INCOMPLETE + UNCHECK
+  socket.on("join-room", function(roomId, userId) {
+    console.log(`${userId} joined room ${roomId}`)
 
     socket.join(roomId)
-    socket.broadcast.to(roomId).emit('user-connected', userId)
-
-    socket.on('ready', () => {
-      console.log("Socket get ready")
-    })
-
-    socket.on('disconnect', () => {
-      console.log("Socket disconnected")
-      socket.to(roomId).emit('user-disconnected', userId)
-    })
+    socket.broadcast.emit("hello", "world")
   })
-})
 
-server.listen(3030);
+  // Call when a client wants to connect to this client.
+  socket.on("connect to user", (message) => {
+    console.log("Got connect offer: ", message)
+  })
+});
