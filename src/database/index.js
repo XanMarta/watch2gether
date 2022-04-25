0dbd9944867399397ee8ccd07aff2a28a5373ea2
XanMarta <gmxancrazy24114@gmail.com>
2022-04-25 18:53:08 +0700

    simple database refactor. use mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,94 +1,42 @@
-const mongoose = require('mongoose')
+const MongoClient = require("mongodb").MongoClient
+const path = require("path")
 
-mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('Connected to MongoDB...'))
+require("dotenv").config({
+    path: path.resolve(__dirname, "../../.env.dev")     // For development
+})
 
-/**
- * User db
- */
-const userSchema = new mongoose.Schema({ 
-});
+const DB_ENDPOINT = process.env.DB_ENDPOINT
+const DB_NAME = "w2g"
+const db = {}
+db.collections = {}
 
-const User = mongoose.model('User', userSchema);
 
-async function user_add() {
-    const user = new User({
-    });
-
-    const result = await user.save();
-    console.log(result);
-    console.log(user._id);
+function _init(db_module) {
+    db.collections[db_module.name] = {}
+    return db_module.func(db.collections[db_module.name])
 }
 
-async function user_remove(id) {
-    const result = await User.deleteOne({ user_id: id});
-    console.log(result);
-}
-
-/**
- * Room db
- */
-const roomSchema = new mongoose.Schema({
-    host: Number,
-    user_list: [Number]
-});
-
-const Room = mongoose.model('Room', roomSchema);
-
-async function room_add() {
-    const room = new Room({
-        host: null,
-        user_list: []
-    });
-
-    const result = await room.save();
-    console.log(result);
-}
-
-async function room_remove(id) {
-    const result = await Room.deleteOne({ room_id: id});
-    console.log(result);
-}
-
-async function room_add_user(room_id, user_id) {
-    const room = await Room.findById(room_id);
-    if (!room) return;
-
-    room.user_list.push(user_id);
-
-    const result = await room.save();
-    console.log(result);
-}
-
-async function room_remove_user(room_id, user_id) {
-    const room = await Room.findById(room_id);
-    if (!room) return;
-
-    var index = room.user_list.indexOf(user_id);
-    if (user_id !== -1) {
-        room.user_list.splice(index, 1);
+db.connect_db = async () => {   // Must be called first
+    console.log("Connecting to database ...")
+    db.client = new MongoClient(DB_ENDPOINT)
+    try {
+        await db.client.connect()
+        console.log("Database connected")
+        db.db = db.client.db(DB_NAME)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
     }
-
-    const result = await room.save();
-    console.log(result);
+    // Init db
+    for (var col_name in db.collections) {
+        db.collections[col_name].collection = db.db.collection(col_name)
+        console.log(`Set collection: ${col_name}`)
+    }
 }
 
-async function room_set_host(room_id, host_id) {
-    const room = await Room.findById(room_id);
-    if (!room) return;
+// Set collection
 
-    room.host = host_id;
+db.Product = _init(require("./product.model"))
 
-    const result = await room.save();
-    console.log(result);
-}
 
-async function room_unset_host(room_id) {
-    const room = await Room.findById(room_id);
-    if (!room) return;
-
-    room.host = null;
-
-    const result = await room.save();
-    console.log(result);
-}
+module.exports = db
