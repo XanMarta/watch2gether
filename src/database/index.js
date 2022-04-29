c5313b6d3073640a2205cd54feeb0ea6806782f2
XanMarta <gmxancrazy24114@gmail.com>
2022-04-29 10:39:14 +0700

    add queries for User and Room db

--- src/database/index.js
+++ src/database/index.js
@@ -9,11 +9,14 @@ const DB_ENDPOINT = process.env.DB_ENDPOINT
 const DB_NAME = "w2g"
 const db = {}
 db.collections = {}
+db.modules = {}
 
 
 function _init(db_module) {
     db.collections[db_module.name] = {}
-    return db_module.func(db.collections[db_module.name])
+    let modules = db_module.func(db.collections[db_module.name])
+    db.modules[db_module.name] = modules
+    return modules
 }
 
 db.connect_db = async () => {   // Must be called first
@@ -29,8 +32,13 @@ db.connect_db = async () => {   // Must be called first
     }
     // Init db
     for (var col_name in db.collections) {
-        db.collections[col_name].collection = db.db.collection(col_name)
         console.log(`Set collection: ${col_name}`)
+        db.collections[col_name].collection = db.db.collection(col_name)
+    }
+    for (var col_name in db.modules) {
+        if (db.modules[col_name]._init) {
+            await db.modules[col_name]._init()
+        }
     }
 }
 
--- src/database/product.model.js
+++ src/database/product.model.js
@@ -3,6 +3,9 @@ module.exports = {
     name: "Product",
     func: (col) => {
         return {
+            _init: async () => {
+                // await col.collection.createIndex({ index: 1 }, { unique: true })
+            },
             add_product: async (name, date, description) => {
                 await col.collection.insertOne({
                     name, date, description
