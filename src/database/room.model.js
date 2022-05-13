3b61629c78ccc0bbe322146c87eba7897c98e129
XanMarta <gmxancrazy24114@gmail.com>
2022-05-13 08:26:01 +0700

    basic orm queries

--- src/database/room.model.js
+++ src/database/room.model.js
@@ -7,52 +7,92 @@ module.exports = {
                 await await col.collection.createIndex({ roomid: 1 }, { unique: true })
             },
 
-            add: async (roomid) => {
-                let data = {
-                    roomid: roomid,
-                    host: null,
-                    users: []
+            // add: async (roomid) => {
+            //     let data = {
+            //         roomid: roomid,
+            //         host: null,
+            //         users: []
+            //     }
+            //     let result = await col.collection.insertOne(data)
+            //     return result.insertedId.toString()
+            // },
+
+            // remove: async (roomid) => {
+            //     let result = await col.collection.deleteOne({ roomid: roomid })
+            //     return result.deletedCount
+            // },
+
+            // add_user: async (roomid, userid) => {
+            //     let result = await col.collection.updateMany(
+            //         { roomid: roomid },
+            //         { $push: { users: userid } }
+            //     )
+            //     return result.modifiedCount
+            // },
+
+            // remove_user: async (roomid, userid) => {
+            //     let result = await col.collection.updateMany(
+            //         { roomid: roomid },
+            //         { $pull: { users: userid } }
+            //     )
+            //     return result.modifiedCount
+            // },
+
+            // set_host: async (roomid, hostid) => {
+            //     let result = await col.collection.updateMany(
+            //         { roomid: roomid },
+            //         { host: hostid }
+            //     )
+            //     return result.modifiedCount
+            // },
+
+            // unset_host: async (roomid) => {
+            //     let result = await col.collection.updateMany(
+            //         { roomid: roomid },
+            //         { host: null }
+            //     )
+            //     return result.modifiedCount
+            // },
+
+            getRoomOwner: async (roomid) => {
+                let result = await col.collection.find({ roomid }).toArray()
+                let host = null
+                if (result.length != 0) {
+                    host = result[0].host
                 }
-                let result = await col.collection.insertOne(data)
-                return result.insertedId.toString()
+                return host
             },
 
-            remove: async (roomid) => {
-                let result = await col.collection.deleteOne({ roomid: roomid })
-                return result.deletedCount
+            isRoomOwner: async (userid, roomid) => {
+                let result = await col.collection.find({ roomid }).toArray()
+                let host = null
+                if (result.length != 0) {
+                    host = result[0].host
+                }
+                return (userid == host)
             },
 
-            add_user: async (roomid, userid) => {
-                let result = await col.collection.updateMany(
-                    { roomid: roomid },
-                    { $push: { users: userid } }
-                )
-                return result.modifiedCount
+            addRoomOwner: async (userid, roomid) => {
+                await col.collection.updateOne({
+                    roomid
+                }, {
+                    $set: {
+                        host: userid
+                    }
+                }, {
+                    upsert: true
+                })
             },
 
-            remove_user: async (roomid, userid) => {
-                let result = await col.collection.updateMany(
-                    { roomid: roomid },
-                    { $pull: { users: userid } }
-                )
-                return result.modifiedCount
+            removeRoomOwner: async (userid, roomid) => {
+                await col.collection.updateOne({
+                    host: userid
+                }, {
+                    $set: {
+                        host: null
+                    }
+                })
             },
-
-            set_host: async (roomid, hostid) => {
-                let result = await col.collection.updateMany(
-                    { roomid: roomid },
-                    { host: hostid }
-                )
-                return result.modifiedCount
-            },
-
-            unset_host: async (roomid) => {
-                let result = await col.collection.updateMany(
-                    { roomid: roomid },
-                    { host: null }
-                )
-                return result.modifiedCount
-            }
         }
     }
 }
--- src/database/user.model.js
+++ src/database/user.model.js
@@ -4,17 +4,62 @@ module.exports = {
     func: (col) => {
         return {
             _init: async () => {
+                await col.collection.createIndex({ socketid: 1}, { unique: true })
                 await col.collection.createIndex({ username: 1 }, { unique: true })
             },
-            add: async (username) => {
-                let result = await col.collection.insertOne({
-                    username: username
-                })
-                return result.insertedId.toString(0)
+
+            setUsername: async (socketid, username) => {
+                try {
+                    let result = await col.collection.insertOne({ socketid, username, roomid: null })
+                    return result.insertedId.toString(0)
+                } catch (error) {
+                    return null
+                }
             },
-            remove: async (username) => {
-                let result = await col.collection.deleteOne({ username: username })
+
+            getUsername: async (socketid) => {
+                let result = await col.collection.find({ socketid }).toArray()
+                let username = null
+                if (result.length != 0) {
+                    username = result[0].username
+                }
+                return username
+            },
+
+            getSocketid: async (username) => {
+                let result = await col.collection.find({ username }).toArray()
+                let socketid = null
+                if (result.length != 0) {
+                    socketid = result[0].socketid
+                }
+                return socketid
+            },
+
+            isUsernameExist: async (username) => {
+                let result = await col.collection.find({ username }).toArray()
+                return (result.length != 0)
+            },
+
+            deleteUsername: async (username) => {
+                let result = await col.collection.deleteOne({ username })
                 return result.deletedCount
+            },
+
+            getRoomId: async (socketid) => {
+                let result = await col.collection.find({ socketid }).toArray()
+                let roomid = null
+                if (result.length != 0) {
+                    roomid = result[0].roomid
+                }
+                return roomid
+            },
+
+            setRoomId: async (socketid, roomid) => {
+                await col.collection.updateOne({
+                    socketid
+                }, {
+                    $set: { roomid }
+                })
             }
         }
     }
