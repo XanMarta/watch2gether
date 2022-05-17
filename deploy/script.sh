58aa2943ed7a5694dcddc72a5120ccdd71e4adc6
XanMarta <gmxancrazy24114@gmail.com>
2022-05-17 11:19:31 +0700

    use github workspace path

--- deploy/script.sh
+++ deploy/script.sh
@@ -5,8 +5,9 @@
 # $3: DOCKERHUB_USERNAME
 # $4: DOCKERHUB_TOKEN
 # $5: TAG
+# $6: KEY_PATH
 
 
-ssh -o StrictHostKeychecking=no -i ~/.ssh/keys $2@$1 rm -rf /home/$2/w2g
-scp -o StrictHostKeychecking=no -i ~/.ssh/keys -r $(dirname $(realpath $0))/script $2@$1:/home/$2/w2g
-ssh -o StrictHostKeychecking=no -i ~/.ssh/keys $2@$1 bash /home/$2/w2g/start.sh $3 $4 $5
+ssh -o StrictHostKeychecking=no -i "$6" $2@$1 rm -rf /home/$2/w2g
+scp -o StrictHostKeychecking=no -i "$6" -r $(dirname $(realpath $0))/script $2@$1:/home/$2/w2g
+ssh -o StrictHostKeychecking=no -i "$6" $2@$1 bash /home/$2/w2g/start.sh $3 $4 $5
