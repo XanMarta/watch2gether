b55c0c82a40581ccd917cafd98d39f9ee64c7dd8
XanMarta <gmxancrazy24114@gmail.com>
2022-05-19 01:18:14 +0700

    test stun server #2

--- public/js/config/peer.config.js
+++ public/js/config/peer.config.js
@@ -11,20 +11,6 @@ export function getPeerConfig() {
                 urls: "stun:stun1.l.google.com:19302"
             },
             {
-                url: "stun:stun2.l.google.com:19302",
-                urls: "stun:stun2.l.google.com:19302"
-            },
-            {
-                url: "stun:numb.viagenie.ca:3478",
-                urls: "stun:numb.viagenie.ca:3478"
-            },
-            {
-                url: 'turn:numb.viagenie.ca',
-                urls: 'turn:numb.viagenie.ca',
-                credential: 'muazkh',
-                username: 'webrtc@live.com'
-            },
-            {
                 url: 'turn:turn.anyfirewall.com:443?transport=tcp',
                 urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
                 credential: 'webrtc',
