753e933c9dafd506195d01cead27ef50d7289592
XanMarta <gmxancrazy24114@gmail.com>
2022-05-19 11:37:26 +0700

    test stun server #5

--- public/js/config/peer.config.js
+++ public/js/config/peer.config.js
@@ -11,8 +11,8 @@ export function getPeerConfig() {
                 urls: "stun:stun1.l.google.com:19302"
             },
             {
-                url: "turn:159.89.204.211:3478",
-                urls: "turn:159.89.204.211:3478",
+                url: "turn:turn-w2g.duckdns.org:3478",
+                urls: "turn:turn-w2g.duckdns.org:3478",
                 credential: "webrtc",
                 username: "webrtc"
             }
